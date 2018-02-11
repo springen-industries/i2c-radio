197fe250ada8ff33931565b7870cf0bb760f1dcd
Christopher Spring <chris@nolaspring.net>
2018-02-11 11:05:30 -0600

    flipped i2c ondata and i2c.begin

--- src/main.cpp
+++ src/main.cpp
@@ -38,8 +38,8 @@ void receiveEvent(){
 }
 
 void setup(){
-  Wire.begin(i2c_address);                // join i2c bus with address as defined above
   Wire.onReceive(receiveEvent);           // register event
+  Wire.begin(i2c_address);                // join i2c bus with address as defined above
   //Serial.begin(serial_baud);                 // run serial at baud as defined above
 
   //initiallize default ppm values
